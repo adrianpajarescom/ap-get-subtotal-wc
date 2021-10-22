84d8329fafef8f0e2d74b94333919fc51e8e6161
adrianpajares.com <92946266+adrianpajarescom@users.noreply.github.com>
2021-10-22 11:18:00 +0200

    Create ap-get-subtotal-wc.php

---
+++ ap-get-subtotal-wc.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name:       adrianpajares.com - Get Subtotal Cart
+ * Plugin URI:        https://adrianpajares.com/
+ * Description:       Plugin to get the subtotal in cart. Shotcode: [ap_get_subtotal]
+ * Version:           1.0
+ * Author:            adrianpajares.com
+ * License:           MIT
+ */
+
+function shortcode_ap_cart_subtotal() {
+
+    global $woocommerce;
+    $sub = $woocommerce->cart->subtotal;
+
+    return $sub;
+
+}
+add_shortcode('ap-cart-subtotal', 'shortcode_ap_cart_subtotal');
